bcf704ea6c1668a6505f9f4cad2d940b82e5d90c
krish <krishkudawala@gmail.com>
2026-09-13 01:00:30 +0530

    add find size of the min node

---
+++ src/Tree1/Size.java
@@ -0,0 +1,22 @@
+package Tree1;
+
+public class Size {
+    public static int size(Node root){
+        if (root==null) return 0;
+        return 1 +size(root.left)+size(root.right);
+    }
+    public static void main(String[] args) {
+        Node a=new Node(1);
+        Node b=new Node(1);
+        Node c=new Node(1);
+        Node d=new Node(1);
+        Node r=new Node(1);
+        Node i=new Node(1);
+
+        a.left=b; a.right=c;
+        b.left=d; b.right=r;
+        c.left=i;
+
+        System.out.println(size(a));
+    }
+}
